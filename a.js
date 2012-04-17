488eefd94d9a9bd886665ee56b811264ea6c6cf5
jfengsky <jfengsky@gmail.com>
2012-04-17 17:09:31 +0800

    ant add

---
+++ a.js
@@ -0,0 +1,13 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: wb-jiangfeng
+ * Date: 12-4-17
+ * Time: обнГ2:11
+ * To change this template use File | Settings | File Templates.
+ */
+
+function a(el){
+    return el;
+}
+
+
---
+++ b.js
@@ -0,0 +1,11 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: wb-jiangfeng
+ * Date: 12-4-17
+ * Time: обнГ2:11
+ * To change this template use File | Settings | File Templates.
+ */
+
+function b(id){
+    return id;
+}
